6c316cfc8b7829de5c893992e84fdbb77fccd2ad
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2025-11-03 10:21:14 +0100

    feat: implement loading component and integrate it into the main app template

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,21 @@
 <template>
-  <u-app>
-    <nuxt-layout>
-      <nuxt-loading-indicator />
-      <nuxt-page />
-    </nuxt-layout>
-  </u-app>
+  <div>
+    <u-app>
+      <nuxt-layout>
+        <nuxt-loading-indicator />
+        <nuxt-page />
+      </nuxt-layout>
+    </u-app>
+    <loading v-if="isLoading" />
+  </div>
 </template>
+
+<script setup lang="ts">
+const isLoading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 2000); // tiny delay for smoothness, optional
+});
+</script>
---
+++ app/components/loading.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center bg-background-light dark:bg-background-dark z-9999">
+    <div class="animate-spin rounded-full h-12 w-12 border-4 border-gray-300 border-t-gray-700"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
